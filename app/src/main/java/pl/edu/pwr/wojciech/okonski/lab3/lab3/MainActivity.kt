7bfc565c3c8ceac644a7f5ebe6a453a74cd80290
Wojtek Okoński <wojteko22.96@gmail.com>
2017-06-03 16:38:28 +0200

    Add moving ball

--- app/src/main/java/pl/edu/pwr/wojciech/okonski/lab3/lab3/MainActivity.kt
+++ app/src/main/java/pl/edu/pwr/wojciech/okonski/lab3/lab3/MainActivity.kt
@@ -1,26 +1,45 @@
 package pl.edu.pwr.wojciech.okonski.lab3.lab3
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.OvalShape
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private val sensorManager: SensorManager by lazy {
+        getSystemService(SENSOR_SERVICE) as SensorManager
+    }
+    private val accelerometer: Sensor by lazy {
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    }
+    private val animatedView: AnimatedView by lazy { AnimatedView(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
+//        setContentView(R.layout.activity_main)
+//        val toolbar = findViewById(R.id.toolbar) as Toolbar
+//        setSupportActionBar(toolbar)
+        setContentView(animatedView)
+    }
 
-//        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(animatedView, accelerometer, SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(animatedView)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -34,11 +53,36 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         val id = item.itemId
-
         if (id == R.id.action_settings) {
             return true
         }
-
         return super.onOptionsItemSelected(item)
+    }
+
+    inner class AnimatedView(context: Context) : ImageView(context), SensorEventListener {
+        private var shapeX: Int = 0
+        private var shapeY: Int = 0
+        private val shapeWidth = 100
+        private val shapeHeight = 100
+        private val shape = ShapeDrawable(OvalShape())
+
+        init {
+            shape.paint.color = Color.GREEN
+        }
+
+        override fun onSensorChanged(event: SensorEvent) {
+            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                shapeX -= event.values[0].toInt()
+                shapeY += event.values[1].toInt()
+            }
+        }
+
+        override fun onDraw(canvas: Canvas) {
+            shape.setBounds(shapeX, shapeY, shapeX + shapeWidth, shapeY + shapeHeight)
+            shape.draw(canvas)
+            invalidate()
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
     }
 }
