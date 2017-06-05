ecac5b6e770e0b97343f06fc1cbeed265d66a158
Wojtek Okoński <wojteko22.96@gmail.com>
2017-06-05 08:58:51 +0200

    Add apple

--- app/src/main/java/pl/edu/pwr/wojciech/okonski/lab3/lab3/ShapeView.kt
+++ app/src/main/java/pl/edu/pwr/wojciech/okonski/lab3/lab3/ShapeView.kt
@@ -10,25 +10,48 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.util.AttributeSet
 import android.widget.ImageView
+import java.util.*
 
 class ShapeView(context: Context, attrs: AttributeSet?) : ImageView(context, attrs), SensorEventListener {
     private var shapeX: Int = 0
     private var shapeY: Int = 0
     private val shapeWidth = 100
     private val shapeHeight = 100
+    private val appleWidth = 30
+    private val appleHeight = 30
     private val shape = ShapeDrawable(OvalShape())
+    private val apple = ShapeDrawable()
+    private val generator = Random()
 
-    init {
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         setShapeColor(Color.GREEN)
+        apple.setColor(Color.BLACK)
+        deployApple()
     }
 
-    fun setShapeColor(newColor: Int) {
-        shape.paint.color = newColor
+    fun setShapeColor(color: Int) {
+        shape.setColor(color)
+    }
+
+    private fun ShapeDrawable.setColor(color: Int) {
+        this.paint.color = color
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        shape.setBounds(shapeX, shapeY, shapeX + shapeWidth, shapeY + shapeHeight)
+        shape.draw(canvas)
+        apple.draw(canvas)
+        invalidate()
     }
 
     override fun onSensorChanged(event: SensorEvent) {
         updateXIfNecessary(event)
         updateYIfNecessary(event)
+//        val number = tvResult.text.toString().toInt()
+//        tvResult.text = (number + 1).toString()
+        if (shape.bounds.contains(apple.bounds))
+            deployApple()
     }
 
     private fun updateXIfNecessary(event: SensorEvent) {
@@ -63,10 +86,10 @@ class ShapeView(context: Context, attrs: AttributeSet?) : ImageView(context, att
 
     private fun squareWithSign(number: Int) = number * Math.abs(number)
 
-    override fun onDraw(canvas: Canvas) {
-        shape.setBounds(shapeX, shapeY, shapeX + shapeWidth, shapeY + shapeHeight)
-        shape.draw(canvas)
-        invalidate()
+    private fun deployApple() {
+        val x = generator.nextInt(measuredWidth - appleWidth)
+        val y = generator.nextInt(measuredHeight - appleHeight)
+        apple.setBounds(x, y, x + appleWidth, y + appleHeight)
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
